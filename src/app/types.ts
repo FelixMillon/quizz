b5d85b293430b573aee25443650ab73719509ac5
FelixMillon <felix.millon@outlook.com>
2024-09-06 11:37:49 +0200

    delete old types

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,32 +1,14 @@
-
-
-// export type Question = {
-//   q: string;
-//   r: string[];
-//   correct: string;
-// };
-
 export interface Question {
   q: string;
   r: string[];
   correct: string;
 }
 
-// export type User = {
-//   name: string;
-//   email: string;
-//   password: string | null;
-// }
-
 export interface User {
   name: string;
   email: string;
   password: string | null;
 }
-
-// export type Response = {
-//   [key: number]: string;
-// }
 
 export interface Response {
   [key: number]: string;
