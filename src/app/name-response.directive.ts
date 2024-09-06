26764604c4915c273f525587263fcf29b836b591
FelixMillon <felix.millon@outlook.com>
2024-09-06 11:35:45 +0200

    no more any

--- src/app/name-response.directive.ts
+++ src/app/name-response.directive.ts
@@ -8,15 +8,15 @@ export class NameResponseDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    // Sélectionner tous les boutons radio dans le document
-    
-    const spans = this.el.nativeElement.querySelectorAll('span');
+    const spans: HTMLSpanElement[] = this.el.nativeElement.querySelectorAll('span');
     let i=0
     spans.forEach(
-      (span: any) =>{
-        let letter = String.fromCharCode('a'.charCodeAt(0) + (i)) + ". "
-        span.lastChild.nodeValue = letter + span.lastChild.nodeValue
-        i++
+      (span: HTMLSpanElement) =>{
+        if(span.lastChild){
+          let letter = String.fromCharCode('a'.charCodeAt(0) + (i)) + ". "
+          span.lastChild.nodeValue = letter + span.lastChild.nodeValue
+          i++
+        }
       }
     )
   }
